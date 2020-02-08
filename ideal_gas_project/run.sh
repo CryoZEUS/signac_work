1d8cee1fd5506a365e37d5ceded170fab05379bc
CryoZEUS <yashbhardwaj911@gmail.com>
2020-02-08 11:03:12 +0530

    Job scripts and cluster submission

---
+++ ideal_gas_project/run.sh
@@ -0,0 +1,60 @@
+
+set -e
+set -u
+
+cd /home/yash/projects/GSOC/signac_work/ideal_gas_project
+
+# store_volume_in_json_file(22a51374466c4e01ef0e67e65f73c52e)
+/usr/bin/python3 project.py exec store_volume_in_json_file 22a51374466c4e01ef0e67e65f73c52e
+
+# store_volume_in_document(22a51374466c4e01ef0e67e65f73c52e)
+/usr/bin/python3 project.py exec store_volume_in_document 22a51374466c4e01ef0e67e65f73c52e
+
+# store_volume_in_json_file(97ac0114bb2269561556b16aef030d43)
+/usr/bin/python3 project.py exec store_volume_in_json_file 97ac0114bb2269561556b16aef030d43
+
+# store_volume_in_document(97ac0114bb2269561556b16aef030d43)
+/usr/bin/python3 project.py exec store_volume_in_document 97ac0114bb2269561556b16aef030d43
+
+# store_volume_in_json_file(a2fa2b860d0a1df3f5dbaaa3a7798a59)
+/usr/bin/python3 project.py exec store_volume_in_json_file a2fa2b860d0a1df3f5dbaaa3a7798a59
+
+# store_volume_in_document(a2fa2b860d0a1df3f5dbaaa3a7798a59)
+/usr/bin/python3 project.py exec store_volume_in_document a2fa2b860d0a1df3f5dbaaa3a7798a59
+
+# store_volume_in_json_file(dbe8094b72da6b3dd7c8f17abdcb7608)
+/usr/bin/python3 project.py exec store_volume_in_json_file dbe8094b72da6b3dd7c8f17abdcb7608
+
+# store_volume_in_document(dbe8094b72da6b3dd7c8f17abdcb7608)
+/usr/bin/python3 project.py exec store_volume_in_document dbe8094b72da6b3dd7c8f17abdcb7608
+
+# store_volume_in_json_file(e5613a5439caeb021ce40a2fc0ebe7ed)
+/usr/bin/python3 project.py exec store_volume_in_json_file e5613a5439caeb021ce40a2fc0ebe7ed
+
+# store_volume_in_document(e5613a5439caeb021ce40a2fc0ebe7ed)
+/usr/bin/python3 project.py exec store_volume_in_document e5613a5439caeb021ce40a2fc0ebe7ed
+
+# store_volume_in_json_file(71855b321a04dd9ee27ce6c9cc0436f4)
+/usr/bin/python3 project.py exec store_volume_in_json_file 71855b321a04dd9ee27ce6c9cc0436f4
+
+# store_volume_in_document(71855b321a04dd9ee27ce6c9cc0436f4)
+/usr/bin/python3 project.py exec store_volume_in_document 71855b321a04dd9ee27ce6c9cc0436f4
+
+# store_volume_in_json_file(742c883cbee8e417bbb236d40aea9543)
+/usr/bin/python3 project.py exec store_volume_in_json_file 742c883cbee8e417bbb236d40aea9543
+
+# store_volume_in_document(742c883cbee8e417bbb236d40aea9543)
+/usr/bin/python3 project.py exec store_volume_in_document 742c883cbee8e417bbb236d40aea9543
+
+# store_volume_in_json_file(ee550647e3f707b251eeb094f43d434c)
+/usr/bin/python3 project.py exec store_volume_in_json_file ee550647e3f707b251eeb094f43d434c
+
+# store_volume_in_document(ee550647e3f707b251eeb094f43d434c)
+/usr/bin/python3 project.py exec store_volume_in_document ee550647e3f707b251eeb094f43d434c
+
+# store_volume_in_json_file(03585df0f87fada67bd0f540c102cce7)
+/usr/bin/python3 project.py exec store_volume_in_json_file 03585df0f87fada67bd0f540c102cce7
+
+# store_volume_in_document(03585df0f87fada67bd0f540c102cce7)
+/usr/bin/python3 project.py exec store_volume_in_document 03585df0f87fada67bd0f540c102cce7
+
