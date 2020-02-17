e234705cc8617af84ff93b7f8ef778322d98a7a1
CryoZEUS <yashbhardwaj911@gmail.com>
2020-02-17 20:35:17 +0530

    Data Space Operations

--- my_project/operations.py
+++ my_project/operations.py
@@ -9,6 +9,8 @@ def hello(job):
 		#with open('hello.txt', 'w') as file:
 			#file.write('world!\n')
 
+
+
 if __name__ == '__main__':
 	import flow
 	flow.run()
--- my_project/run.py
+++ my_project/run.py
@@ -4,7 +4,13 @@ import signac
 from operations import hello
 project = signac.get_project()
 
+
+def greeted(job):
+	return job.isfile('hello.txt')
+
+	
 for job in project:
-	hello(job)
+	if not greeted(job):
+		hello(job)
 
 	
